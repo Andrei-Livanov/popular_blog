569c916b4f7f99143bcde7e9fbf36277af84ffe2
andrei <livanovandrej99@gmail.com>
2023-04-05 16:44:06 +0700

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import Grid from '@mui/material/Grid';
+
+import { fetchPosts, fetchTags } from '../redux/slices/posts';
+import { CommentsBlock, Post, TagsBlock } from '../components';
+
+export const Home = () => {
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.auth.data);
+  const { posts, tags } = useSelector((state) => state.posts);
+
+  const isPostsLoading = posts.status === 'loading';
+  const isTagsLoading = tags.status === 'loading';
+
+  React.useEffect(() => {
+    dispatch(fetchPosts());
+    dispatch(fetchTags());
+  }, []);
+
+  return (
+    <>
+      <Tabs style={{ marginBottom: 15 }} value={0} aria-label="basic tabs example">
+        <Tab label="Новые" />
+        <Tab label="Популярные" />
+      </Tabs>
+      <Grid container spacing={4}>
+        <Grid xs={8} item>
+          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+            isPostsLoading ? (
+              <Post key={index} isLoading={true} />
+            ) : (
+              <Post
+                id={obj._id}
+                title={obj.title}
+                imageUrl={obj.imageUrl ? `https://blog-backend-lac.vercel.app${obj.imageUrl}` : ''}
+                user={obj.user}
+                createdAt={obj.createdAt}
+                viewsCount={obj.viewsCount}
+                commentsCount={3}
+                tags={obj.tags}
+                isEditable={userData?._id === obj.user._id}
+              />
+            )
+          )}
+        </Grid>
+        <Grid xs={4} item>
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
+          <CommentsBlock
+            items={[
+              {
+                user: {
+                  fullName: 'Фёдор Попов',
+                  avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
+                },
+                text: 'Это тестовый комментарий',
+              },
+              {
+                user: {
+                  fullName: 'Иван Васильевич',
+                  avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
+                },
+                text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
+              },
+            ]}
+            isLoading={false}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import { authReducer } from './slices/auth';
+import { postsReducer } from './slices/posts';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    posts: postsReducer,
+  },
+});
+
+export default store;
